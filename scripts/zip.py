18c624b112d47aca2c4989afbc4ca81d460af9c6
S. Hutchins <ms.s.hutchins@gmail.com>
2019-04-30 14:58:20 -0500

    Add archive format as zip.

--- scripts/zip.py
+++ scripts/zip.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 import shutil
 
-shutil.make_archive(snakemake.input.folder, snakemake.output.outfile)
+shutil.make_archive(snakemake.input.folder, "zip", snakemake.output.outfile)
